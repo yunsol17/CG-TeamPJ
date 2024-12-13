82aba886a82dff73ee6a2556425df71d51c4d769
juhwanTV <0412juhwan@gmail.com>
2024-12-13 13:06:00 +0900

    중간커밋

--- CG-TeamProject/CG-TeamProject/main.cpp
+++ CG-TeamProject/CG-TeamProject/main.cpp
@@ -504,7 +504,6 @@ void InitCheckBoxMap5() {
     glBindVertexArray(0);
 }
 
-
 // ¸Ê ±×¸®±â
 void DrawMap(GLuint shaderPRogramID, GLint modelMatrixLocation) {
     // ¹Ù´Ú
@@ -688,11 +687,11 @@ void DrawCharacter2(GLuint shaderProgramID, GLint modelMatrixLocation) {
 }
 
 void DrawMapCheckBox(GLuint shaderProgramID, GLint modelMatrixLocation) {
-    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
     glm::mat4 checkBoxModelMatrix1 = glm::mat4(1.0f);
     glUniformMatrix4fv(modelMatrixLocation, 1, GL_FALSE, glm::value_ptr(checkBoxModelMatrix1));
     glBindVertexArray(vaoCheckBoxMap1);
-        glDrawArrays(GL_QUADS, 0, 24); 
+    glDrawArrays(GL_QUADS, 0, 24); 
     glBindVertexArray(0);
 
     glm::mat4 checkBoxModelMatrix2 = glm::mat4(1.0f);
@@ -718,9 +717,6 @@ void DrawMapCheckBox(GLuint shaderProgramID, GLint modelMatrixLocation) {
     glBindVertexArray(vaoCheckBoxMap5);
     glDrawArrays(GL_QUADS, 0, 24);
     glBindVertexArray(0);
-
-
-
 }
 
 void main(int argc, char** argv) {
